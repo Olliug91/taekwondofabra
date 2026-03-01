4e8a51f3b3544021e43a89ccfc1405ca936e7d5f
Olliug91 <guitumar91@gmail.com>
2026-03-01 18:53:27 +0100

    UPDATE liga hockey manopla

--- resources/views/pages/hockey-manopla.blade.php
+++ resources/views/pages/hockey-manopla.blade.php
@@ -739,14 +739,14 @@
                 <td>27/02/2026</td>
                 <td class="center"><span class="pill">J2</span></td>
                 <td><strong>🐉 Dragones</strong> vs <strong>🐺 Lobos</strong><br><span class="small">Resultado:</span>
-                  <strong>__</strong> - <strong>__</strong><br><span class="small">Puntos:</span> 🐉 <strong>__</strong>
+                  <strong>0</strong> - <strong>1</strong><br><span class="small">Puntos:</span> 🐉 <strong>0</strong>
                   |
-                  🐺 <strong>__</strong>
+                  🐺 <strong>3</strong>
                 </td>
                 <td><strong>🦅 Halcones</strong> vs <strong>🐯 Tigres</strong><br><span class="small">Resultado:</span>
-                  <strong>__</strong> - <strong>__</strong><br><span class="small">Puntos:</span> 🦅 <strong>__</strong>
+                  <strong>1</strong> - <strong>2</strong><br><span class="small">Puntos:</span> 🦅 <strong>0</strong>
                   |
-                  🐯 <strong>__</strong>
+                  🐯 <strong>3</strong>
                 </td>
               </tr>
 
@@ -1045,50 +1045,50 @@
               <tr>
                 <td class="center">1</td>
                 <td>🐉 Dragones</td>
-                <td class="center">8</td>
+                <td class="center">9</td>
                 <td class="center">6</td>
                 <td class="center">1</td>
-                <td class="center">1</td>
+                <td class="center">2</td>
                 <td class="center">19</td>
-                <td class="center">11</td>
-                <td class="center">+8</td>
+                <td class="center">12</td>
+                <td class="center">+7</td>
                 <td class="center">19</td>
               </tr>
               <tr>
                 <td class="center">2</td>
                 <td>🐺 Lobos</td>
+                <td class="center">7</td>
                 <td class="center">6</td>
-                <td class="center">5</td>
                 <td class="center">0</td>
                 <td class="center">1</td>
-                <td class="center">12</td>
+                <td class="center">13</td>
                 <td class="center">9</td>
-                <td class="center">+3</td>
-                <td class="center">15</td>
+                <td class="center">+4</td>
+                <td class="center">18</td>
               </tr>
               <tr>
                 <td class="center">3</td>
                 <td>🦅 Halcones</td>
-                <td class="center">7</td>
+                <td class="center">8</td>
                 <td class="center">1</td>
                 <td class="center">2</td>
-                <td class="center">4</td>
-                <td class="center">11</td>
-                <td class="center">14</td>
-                <td class="center">-3</td>
+                <td class="center">5</td>
+                <td class="center">12</td>
+                <td class="center">16</td>
+                <td class="center">-4</td>
                 <td class="center">5</td>
               </tr>
               <tr>
                 <td class="center">4</td>
                 <td>🐯 Tigres</td>
-                <td class="center">7</td>
-                <td class="center">0</td>
+                <td class="center">8</td>
+                <td class="center">1</td>
                 <td class="center">1</td>
                 <td class="center">6</td>
-                <td class="center">15</td>
-                <td class="center">23</td>
-                <td class="center">-8</td>
-                <td class="center">1</td>
+                <td class="center">17</td>
+                <td class="center">24</td>
+                <td class="center">-7</td>
+                <td class="center">4</td>
               </tr>
             </tbody>
           </table>
@@ -1117,7 +1117,7 @@
               </tr>
               <tr>
                 <td>Miguel Fernandez (Halcones)</td>
-                <td class="center">6</td>
+                <td class="center">7</td>
               </tr>
               <tr>
                 <td>Jesus Lopez Gonzalez (Dragones)</td>
@@ -1128,11 +1128,11 @@
                 <td class="center">5</td>
               </tr>
               <tr>
-                <td>Nouhaila Laagel (Tigres)</td>
-                <td class="center">3</td>
+                <td>Nicolas Castro (Dragones)</td>
+                <td class="center">4</td>
               </tr>
               <tr>
-                <td>Nicolas Castro (Dragones)</td>
+                <td>Nouhaila Laagel (Tigres)</td>
                 <td class="center">3</td>
               </tr>
               <tr>
@@ -1145,6 +1145,10 @@
               </tr>
               <tr>
                 <td>Rodrigo Lopez (Tigres)</td>
+                <td class="center">2</td>
+              </tr>
+              <tr>
+                <td>Vicente Castro (Tigres)</td>
                 <td class="center">2</td>
               </tr>
               <tr>
@@ -1164,7 +1168,7 @@
                 <td class="center">1</td>
               </tr>
               <tr>
-                <td>Vicente Castro (Tigres)</td>
+                <td>Alula Rawat (Lobos)</td>
                 <td class="center">1</td>
               </tr>
             </tbody>
